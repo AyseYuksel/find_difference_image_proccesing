81e8256b65a21c05d8fab5604419bef83f7ec57c
AyseYuksel <68689527+AyseYuksel@users.noreply.github.com>
2021-06-21 06:33:58 -0700

    initial commit

---
+++ find_difference.m
@@ -0,0 +1,15 @@
+﻿resim1=imread("resim1.jpg");
+resim2=imread("resim2.jpg");
+griresim1=rgb2gray(resim1);
+griresim2=rgb2gray(resim2);
+c=double(griresim1)-double(griresim2);
+
+c=abs(c);
+%min(min(c)); %0
+
+ c=uint8(c); 
+ 
+ c=double(c);
+ fark=(c>1)*255;
+ gercekfark=(fark/255).*griresim2;
+ imshow(gercekfark)
